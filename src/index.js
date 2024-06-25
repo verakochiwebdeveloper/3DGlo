3df8253e0f4e5378ab4292937fb33a201d8517ef
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-06-25 07:56:39 +0300

    slider js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import menu from './modules/menu';
 import modal from './modules/modal';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 menu()
 modal()
 tabs()
+slider()
